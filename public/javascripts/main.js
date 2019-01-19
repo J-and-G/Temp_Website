cb3da9e42ab7b2cdc38bb95ad2b0167be074b47e
abhimanyu891998 <singhabhimanyu186@gmail.com>
2019-01-20 01:58:08 +0800

    First day end

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -35,35 +35,18 @@
       $(".loading-page .counter h1.color").css("width", c + "%");
       */
       counter++;
-      if(counter == 101) {
-		  counter=100;
+      if(counter === 101) {
+		  counter=0;
 		  clearInterval(i);
-		  $('.loading-page').fadeOut(1500, ()=>{
-			$("#home").delay(300).fadeIn("slow");
+		  $('.loading-page').fadeOut("slow",()=>{
+			//$("#home").delay("slow").fadeIn("slow");
+			$("#home").css('display','block').hide().fadeIn("slow");
 		  })
+		 
       }
     }, estimatedTime/100);
 
-	var counter = 0;
-	var c = 0;
-    var perfData = window.performance.timing // The PerformanceTiming interface
-    var estimatedTime = -(perfData.loadEventEnd - perfData.navigationStart) // Calculated Estimated Time of Page Load which returns negative value.
-    var time = parseInt((estimatedTime/1000)%60)*100; //Converting EstimatedTime from miliseconds to seconds.
-    var i = setInterval(function(){
-        $(".loading-page .counter h1").html(c + "%");
-        $(".loading-page .counter hr").css("width", c + "%");
-        //$(".loading-page .counter").css("background", "linear-gradient(to right, #f60d54 "+ c + "%,#0d0d0d "+ c + "%)");
-  
-      /*
-      $(".loading-page .counter h1.color").css("width", c + "%");
-      */
-      counter++;
-      c++;
-        
-      if(counter == 101) {
-          clearInterval(i);
-      }
-    }, estimatedTime/100);
+
 
 
   	/*---------------------------------------------------- */
